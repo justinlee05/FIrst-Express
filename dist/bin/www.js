d9a03c2b286182405869e84af7a2e45677e6478b
justinlee05 <justinlee05@naver.com>
2022-07-29 12:34:51 +0900

    Structure

--- dist/bin/www.js
+++ dist/bin/www.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
+var app_1 = require("../app");
 var http_1 = require("http");
 var port = Number(process.env.PORT) || 3000;
 var server = (0, http_1.createServer)(app_1.default);
